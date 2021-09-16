aa34264a5033257a7cb1d8dae12c72d3a1224352
jangminjun <jangminjun@gmail.com>
2021-09-16 17:56:01 +0900

    Revert "camel Lab"
    This reverts commit 4d681bd80be011628c5a8629cb429dbeb7f7c00c.

--- camel-labs-1.0-exercise/2_Core_Engine/src/main/java/com/redhat/gpte/training/camel/HelloBean.java
+++
@@ -1,8 +0,0 @@
-package com.redhat.gpte.training.camel;
-
-public class HelloBean {
-    /*
-     * Add a method to say Hello
-     * Use as input the parameter the body received and concatenate it with "Hello World ! " text
-     */
-}
--- camel-labs-1.0-exercise/4_Camel_Project_Development/camel-spring-standalone/src/main/java/com/redhat/gpte/training/camel/bean/MyBean.java
+++
@@ -1,7 +0,0 @@
-package com.redhat.gpte.training.camel.bean;
-
-import org.apache.camel.Body;
-
-public class MyBean {
-
-}
